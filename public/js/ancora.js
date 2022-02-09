5ea90c8607ffb775ee86522934e267614f5b4b10
Zoker <flaviogabriel2603@gmail.com>
2022-02-08 22:56:05 -0300

    commit

--- public/js/ancora.js
+++ public/js/ancora.js
@@ -15,26 +15,80 @@ $('.botao-sobre').click(function() {
 });
 
 $(function() {
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() < 797) {
-            $(".inicio").addClass("cor-menu");
-        } else {
-            $(".inicio").removeClass("cor-menu");
-        }
-        if($(window).scrollTop() > 796 && $(window).scrollTop() < 1406) {
-            $(".sobre").addClass("cor-menu");
-        } else {
-            $(".sobre").removeClass("cor-menu");
-        }
-        if($(window).scrollTop() > 1405 && $(window).scrollTop() < 2089) {
-            $(".hab").addClass("cor-menu");
-        } else {
-            $(".hab").removeClass("cor-menu");
-        }
-        if($(window).scrollTop() > 2088) {
-            $(".contato").addClass("cor-menu");
-        } else {
-            $(".contato").removeClass("cor-menu");
-        }
-    });
+    let largura = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+    if(largura > 768){
+        $(window).on("scroll", function() {
+            
+            if($(window).scrollTop() < 797) {
+                $(".inicio").addClass("cor-menu");
+            } else {
+                $(".inicio").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 796 && $(window).scrollTop() < 1406) {
+                $(".sobre").addClass("cor-menu");
+            } else {
+                $(".sobre").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 1405 && $(window).scrollTop() < 2089) {
+                $(".hab").addClass("cor-menu");
+            } else {
+                $(".hab").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 2088) {
+                $(".contato").addClass("cor-menu");
+            } else {
+                $(".contato").removeClass("cor-menu");
+            }
+        });
+    } else if(largura > 684) {
+        $(window).on("scroll", function() {
+            
+            if($(window).scrollTop() < 613) {
+                $(".inicio").addClass("cor-menu");
+            } else {
+                $(".inicio").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 612 && $(window).scrollTop() < 1140) {
+                $(".sobre").addClass("cor-menu");
+            } else {
+                $(".sobre").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 1139 && $(window).scrollTop() < 1819) {
+                $(".hab").addClass("cor-menu");
+            } else {
+                $(".hab").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 1818) {
+                $(".contato").addClass("cor-menu");
+            } else {
+                $(".contato").removeClass("cor-menu");
+            }
+        });
+    } else {
+        $(window).on("scroll", function() {
+            
+            if($(window).scrollTop() < 613) {
+                $(".inicio").addClass("cor-menu");
+            } else {
+                $(".inicio").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 612 && $(window).scrollTop() < 1148) {
+                $(".sobre").addClass("cor-menu");
+            } else {
+                $(".sobre").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 1147 && $(window).scrollTop() < 1827) {
+                $(".hab").addClass("cor-menu");
+            } else {
+                $(".hab").removeClass("cor-menu");
+            }
+            if($(window).scrollTop() > 1826) {
+                $(".contato").addClass("cor-menu");
+            } else {
+                $(".contato").removeClass("cor-menu");
+            }
+        });
+    }
   });
